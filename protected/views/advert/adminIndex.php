98fdd4124bd0c1c79a112cade83c24e94cfd7302
Владислав <vladis1ove81@gmail.com>
2015-11-25 03:07:11 +0600

    no message

--- protected/views/advert/adminIndex.php
+++ protected/views/advert/adminIndex.php
@@ -19,7 +19,7 @@
 		<div class="b-group-vars">
 	        <? foreach ($data['Attr'][58] as $key => $vars): ?>
 	            <div class="b-group-col">
-	                <?=CHTML::checkBoxList("Attr[58]", $_GET['Attr'][58], $vars, array("separator"=>"","template"=>"<div>{input}{label}</div>")); ?>
+	                <?=CHTML::checkBoxList("Attr[58]", $_GET['Attr'][58], $vars, array("separator"=>"","baseID"=>"arr".$key,"template"=>"<div>{input}{label}</div>")); ?>
 	            </div>
 	        <? endforeach; ?>
 	    </div>
