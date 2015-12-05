4e69e57f3a284f385501f1cf761d5668d90c3959
Владислав <vladis1ove81@gmail.com>
2015-12-05 17:37:23 +0600

    no message

--- protected/views/kolesoOnline/_list.php
+++ protected/views/kolesoOnline/_list.php
@@ -1,5 +1,5 @@
 <? foreach ($goods as $key => $good): ?>   
-    <li  <? unset($_GET['partial'],$_GET['GoodFilter_page'],$_GET['last']); $_GET['id'] = $good->fields_assoc[3]->value; if($key == (count($goods)-1) ) echo "data-last='".$last."'" ?> class="gradient-grey">
+    <li  <? $_GET['type'] = $type; unset($_GET['partial'],$_GET['GoodFilter_page'],$_GET['last']); $_GET['id'] = $good->fields_assoc[3]->value; if($key == (count($goods)-1) ) echo "data-last='".$last."'" ?> class="gradient-grey">
         <div class="good-img" style="background-image: url(<? $images = $this->getImages($good); echo $images[0];?>);"></div>
         <div class="params-cont">
             <a href="<?=Yii::app()->createUrl('/kolesoOnline/detail',$_GET)?>">
