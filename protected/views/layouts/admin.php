38e798ce95c418bb030916580f5b9c0d4be22d66
Михаил <beatbox787@gmail.com>
2015-12-13 04:47:02 +0600

    no message

--- protected/views/layouts/admin.php
+++ protected/views/layouts/admin.php
@@ -16,6 +16,23 @@
     <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/js/plupload/jquery.plupload.queue/css/jquery.plupload.queue.css" />
     <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/admin.css" />
 
+    <link rel="apple-touch-icon-precomposed" sizes="57x57" href="<?php echo Yii::app()->request->baseUrl; ?>/html/icon/admin/apple-touch-icon-57x57.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php echo Yii::app()->request->baseUrl; ?>/html/icon/admin/apple-touch-icon-114x114.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php echo Yii::app()->request->baseUrl; ?>/html/icon/admin/apple-touch-icon-72x72.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?php echo Yii::app()->request->baseUrl; ?>/html/icon/admin/apple-touch-icon-144x144.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="60x60" href="<?php echo Yii::app()->request->baseUrl; ?>/html/icon/admin/apple-touch-icon-60x60.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="120x120" href="<?php echo Yii::app()->request->baseUrl; ?>/html/icon/admin/apple-touch-icon-120x120.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="76x76" href="<?php echo Yii::app()->request->baseUrl; ?>/html/icon/admin/apple-touch-icon-76x76.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="152x152" href="<?php echo Yii::app()->request->baseUrl; ?>/html/icon/admin/apple-touch-icon-152x152.png" />
+    <link rel="icon" type="image/png" href="<?php echo Yii::app()->request->baseUrl; ?>/html/icon/admin/favicon-96x96.png" sizes="96x96" />
+    <link rel="icon" type="image/png" href="<?php echo Yii::app()->request->baseUrl; ?>/html/icon/admin/favicon-32x32.png" sizes="32x32" />
+    <link rel="icon" type="image/png" href="<?php echo Yii::app()->request->baseUrl; ?>/html/icon/admin/favicon-16x16.png" sizes="16x16" />
+    <link rel="icon" type="image/png" href="<?php echo Yii::app()->request->baseUrl; ?>/html/icon/admin/favicon-128.png" sizes="128x128" />
+    <meta name="application-name" content="Колесо Онлайн"/>
+    <meta name="msapplication-TileColor" content="#FFFFFF" />
+    <meta name="msapplication-TileImage" content="<?php echo Yii::app()->request->baseUrl; ?>/html/icon/admin/mstile-144x144.png" />
+    <meta name="msapplication-square70x70logo" content="<?php echo Yii::app()->request->baseUrl; ?>/html/icon/admin/mstile-70x70.png" />
+
 	<script type="text/javascript" src="<?php echo Yii::app()->request->baseUrl; ?>/js/jquery-2.1.1.min.js"></script>
     <script type="text/javascript" src="<?php echo Yii::app()->request->baseUrl; ?>/js/jquery.qtip.min.js"></script>
     <script type="text/javascript" src="<?php echo Yii::app()->request->baseUrl; ?>/js/jquery-ui.min.js"></script>
--- protected/views/layouts/kolesoOnline.php
+++ protected/views/layouts/kolesoOnline.php
@@ -38,7 +38,23 @@ $mobile = (preg_match('/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|
     <meta property="twitter:image" content="<?=$this->image?>">
     <link rel="image_src" href="<?=$this->image?>">
 
-    <link rel="icon" type="image/vnd.microsoft.icon" href="favicon.ico">
+    <link rel="apple-touch-icon-precomposed" sizes="57x57" href="<?php echo Yii::app()->request->baseUrl; ?>/html/icon/apple-touch-icon-57x57.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php echo Yii::app()->request->baseUrl; ?>/html/icon/apple-touch-icon-114x114.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php echo Yii::app()->request->baseUrl; ?>/html/icon/apple-touch-icon-72x72.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?php echo Yii::app()->request->baseUrl; ?>/html/icon/apple-touch-icon-144x144.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="60x60" href="<?php echo Yii::app()->request->baseUrl; ?>/html/icon/apple-touch-icon-60x60.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="120x120" href="<?php echo Yii::app()->request->baseUrl; ?>/html/icon/apple-touch-icon-120x120.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="76x76" href="<?php echo Yii::app()->request->baseUrl; ?>/html/icon/apple-touch-icon-76x76.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="152x152" href="<?php echo Yii::app()->request->baseUrl; ?>/html/icon/apple-touch-icon-152x152.png" />
+    <link rel="icon" type="image/png" href="<?php echo Yii::app()->request->baseUrl; ?>/html/icon/favicon-96x96.png" sizes="96x96" />
+    <link rel="icon" type="image/png" href="<?php echo Yii::app()->request->baseUrl; ?>/html/icon/favicon-32x32.png" sizes="32x32" />
+    <link rel="icon" type="image/png" href="<?php echo Yii::app()->request->baseUrl; ?>/html/icon/favicon-16x16.png" sizes="16x16" />
+    <link rel="icon" type="image/png" href="<?php echo Yii::app()->request->baseUrl; ?>/html/icon/favicon-128.png" sizes="128x128" />
+    <meta name="application-name" content="Колесо Онлайн"/>
+    <meta name="msapplication-TileColor" content="#FFFFFF" />
+    <meta name="msapplication-TileImage" content="<?php echo Yii::app()->request->baseUrl; ?>/html/icon/mstile-144x144.png" />
+    <meta name="msapplication-square70x70logo" content="<?php echo Yii::app()->request->baseUrl; ?>/html/icon/mstile-70x70.png" />
+
 </head>
 <body>
     <ul class="ps-lines">
