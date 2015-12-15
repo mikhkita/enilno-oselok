a39602f36bbbd5837891271bafdf70f91ee9f96e
Владислав <vladis1ove81@gmail.com>
2015-12-16 00:49:50 +0600

    no message

--- protected/views/kolesoOnline/_list.php
+++ protected/views/kolesoOnline/_list.php
@@ -6,7 +6,7 @@
                 <a class="params-cont-a" href="<?=Yii::app()->createUrl('/kolesoOnline/detail',array('id' => $good->fields_assoc[3]->value,'type' => $type))?>">
                 <? if($type == 2): ?>
                     <h4><?=$good->fields_assoc[6]->value?></h4>
-                    <? $price = ($good->fields_assoc[20])?$good->fields_assoc[20]->value:0; $order = Interpreter::generate($this->params[$type]["ORDER"], $good); ?>
+                    <? $price = ($good->fields_assoc[20])?$good->fields_assoc[20]->value:0; ?>
                     <h5><span><?=$price==0 ? Yii::app()->params["zeroPrice"] : number_format( $price, 0, ',', ' ' )." р."?></span> <?=Interpreter::generate($this->params[$type]["SHIPPING"], $good,$dynamic);?></h5>
                     <h5><?=Interpreter::generate($this->params[$type]["AVAILABLE"], $good,$dynamic);?></h5>
                     <h6><?=Interpreter::generate($this->params[$type]["TITLE_2_CODE"], $good,$dynamic);?></h6>
@@ -16,12 +16,21 @@
                     <!-- <h3>Износ: <span>82%</span></h3> -->
                 <? elseif($type == 1): ?>
                     <h4><?=$good->fields_assoc[16]->value." ".$good->fields_assoc[17]->value?></h4>
-                    <? $price = ($good->fields_assoc[20])?$good->fields_assoc[20]->value:0; $order = Interpreter::generate($this->params[$type]["ORDER"], $good); ?>
+                    <? $price = ($good->fields_assoc[20])?$good->fields_assoc[20]->value:0; ?>
                     <h5><span><?=$price==0 ? Yii::app()->params["zeroPrice"] : number_format( $price, 0, ',', ' ' )." р."?> </span> <?=Interpreter::generate($this->params[$type]["SHIPPING"], $good,$dynamic);?></h5>
                     <h5><?=Interpreter::generate($this->params[$type]["AVAILABLE"], $good,$dynamic);?></h5>
                     <h6><?=Interpreter::generate($this->params[$type]["TITLE_2_CODE"], $good,$dynamic);?> <?=$good->fields_assoc[$params[$type]["CATEGORY"]["AMOUNT"]['ID']]->value." ".$params[$type]["CATEGORY"]["AMOUNT"]['UNIT']?></h6>
                     <h3><?=$params[$type]["CATEGORY"]["WEAR"]["LABEL"]?>: <span><?=$good->fields_assoc[$params[$type]["CATEGORY"]["WEAR"]['ID']]->value?> %</span></h3>
                     <h3><?=$params[$type]["CATEGORY"]["YEAR"]["LABEL"]?>: <span><?=$good->fields_assoc[$params[$type]["CATEGORY"]["YEAR"]['ID']]->value?></h3>
+                <? elseif($type == 3): ?>
+                    <h4><?=Interpreter::generate($this->params[$type]["TITLE_CATEGORY"], $good,$dynamic);?></h4>
+                    <? $price = ($good->fields_assoc[20])?$good->fields_assoc[20]->value:0; ?>
+                    <h5><span><?=$price==0 ? Yii::app()->params["zeroPrice"] : number_format( $price, 0, ',', ' ' )." р."?></span> <?=Interpreter::generate($this->params[$type]["SHIPPING"], $good,$dynamic);?></h5>
+                    <h5><?=Interpreter::generate($this->params[$type]["AVAILABLE"], $good,$dynamic);?></h5>
+                    <h6><?=Interpreter::generate($this->params[$type]["TITLE_2_CODE"], $good,$dynamic);?></h6>
+                    <h3>Состояние: <span><?=$good->fields_assoc[26]->value?></span></h3>
+                    <h3><?=$good->fields_assoc[$params[$type]["CATEGORY"]["AMOUNT"]['ID']]->value." ".$params[$type]["CATEGORY"]["AMOUNT"]['UNIT']?></h3>
+                    <h3>Страна: <span><?=(($good->fields_assoc[11])?$good->fields_assoc[11]->value:"Не указано")?></span></h3>
                 <? endif; ?>
                 </a>
                 <a href="#" class="fancy b-orange-butt" data-block="#b-popup-buy" data-aftershow="category_buy">Купить</a>
