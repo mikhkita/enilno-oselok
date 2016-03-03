7f4b4d5a23f9c0c21f9550a8d2e3f04f498b06fe
Михаил <beatbox787@gmail.com>
2016-03-03 17:37:58 +0600

    no message

--- protected/kolesoOnline.php
+++ protected/kolesoOnline.php
@@ -262,23 +262,23 @@ $mobile = (preg_match('/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|
     </div>
 </div>
 
-<script type="text/javascript" src="/html/js/jquery-2.1.1.min.js"></script>
-<script type="text/javascript" src="/html/js/jquery.fancybox.js"></script>
+<script type="text/javascript" src="<?php echo Yii::app()->request->baseUrl; ?>/html/js/jquery-2.1.1.min.js"></script>
+<script type="text/javascript" src="<?php echo Yii::app()->request->baseUrl; ?>/html/js/jquery.fancybox.js"></script>
 <? if( $mobile ): ?>
-<script type="text/javascript" src="/html/js/TweenMax.min.js"></script>
-<script type="text/javascript" src="/html/js/fastclick.js"></script>
+<script type="text/javascript" src="<?php echo Yii::app()->request->baseUrl; ?>/html/js/TweenMax.min.js"></script>
+<script type="text/javascript" src="<?php echo Yii::app()->request->baseUrl; ?>/html/js/fastclick.js"></script>
 <? endif; ?>
-<script type="text/javascript" src="/html/js/jquery.maskedinput.min.js"></script>
-<script type="text/javascript" src="/html/js/jquery.validate.min.js"></script>
-<script type="text/javascript" src="/html/js/slick.min.js"></script>
-<script type="text/javascript" src="/html/js/jquery-ui.min.js"></script>
-<script type="text/javascript" src="/html/js/jquery.ui.touch-punch.js"></script>
-<script type="text/javascript" src="/html/js/device.js"></script>
-<script type="text/javascript" src="/html/js/select2.full.min.js"></script>
-<script type="text/javascript" src="/html/js/stroll.js"></script>
-<script type="text/javascript" src="/html/js/i18n/ru.js"></script>
-<script type="text/javascript" src="/html/js/KitSend.js"></script>
-<script type="text/javascript" src="/html/js/main.js"></script>
+<script type="text/javascript" src="<?php echo Yii::app()->request->baseUrl; ?>/html/js/jquery.maskedinput.min.js"></script>
+<script type="text/javascript" src="<?php echo Yii::app()->request->baseUrl; ?>/html/js/jquery.validate.min.js"></script>
+<script type="text/javascript" src="<?php echo Yii::app()->request->baseUrl; ?>/html/js/slick.min.js"></script>
+<script type="text/javascript" src="<?php echo Yii::app()->request->baseUrl; ?>/html/js/jquery-ui.min.js"></script>
+<script type="text/javascript" src="<?php echo Yii::app()->request->baseUrl; ?>/html/js/jquery.ui.touch-punch.js"></script>
+<script type="text/javascript" src="<?php echo Yii::app()->request->baseUrl; ?>/html/js/device.js"></script>
+<script type="text/javascript" src="<?php echo Yii::app()->request->baseUrl; ?>/html/js/select2.full.min.js"></script>
+<script type="text/javascript" src="<?php echo Yii::app()->request->baseUrl; ?>/html/js/stroll.js"></script>
+<script type="text/javascript" src="<?php echo Yii::app()->request->baseUrl; ?>/html/js/i18n/ru.js"></script>
+<script type="text/javascript" src="<?php echo Yii::app()->request->baseUrl; ?>/html/js/KitSend.js"></script>
+<script type="text/javascript" src="<?php echo Yii::app()->request->baseUrl; ?>/html/js/main.js"></script>
 <? if(isset(Yii::app()->params['server']) && Yii::app()->params['server'] === true): ?>
 <!-- Yandex.Metrika counter --><script type="text/javascript"> (function (d, w, c) { (w[c] = w[c] || []).push(function() { try { w.yaCounter34102235 = new Ya.Metrika({ id:34102235, clickmap:true, trackLinks:true, accurateTrackBounce:true, webvisor:true }); } catch(e) { } }); var n = d.getElementsByTagName("script")[0], s = d.createElement("script"), f = function () { n.parentNode.insertBefore(s, n); }; s.type = "text/javascript"; s.async = true; s.src = "https://mc.yandex.ru/metrika/watch.js"; if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); } })(document, window, "yandex_metrika_callbacks");</script><noscript><div><img src="https://mc.yandex.ru/watch/34102235" style="position:absolute; left:-9999px;" alt="" /></div></noscript><!-- /Yandex.Metrika counter -->
 <? endif; ?>
